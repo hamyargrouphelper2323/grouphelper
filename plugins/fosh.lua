8acd5a1c2d2bd76adfc7f468432279aecccca291
BH-YAGHI <iranchild1@gmail.com>
2016-02-04 00:02:03 +0000

    Create fosh.lua

---
+++ plugins/fosh.lua
@@ -0,0 +1,17 @@
+do
+
+function run(msg, matches)
+  return "سیکتیر تا ننتو نگاییدم " .. matches[1]
+end
+
+return {
+  description = "Says hello to someone", 
+  usage = "say hello to [name]",
+  patterns = {
+    "^سیک (.*)$",
+    "^سیک (.*)$"
+  }, 
+  run = run 
+}
+
+end
