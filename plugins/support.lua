dafbf36d8367254bd5596a3435395242dd0326b3
BH-YAGHI <iranchild1@gmail.com>
2016-02-04 00:13:50 +0000

    Create support.lua

---
+++ plugins/support.lua
@@ -0,0 +1,21 @@
+do
+
+function run(msg, matches)
+  return " The Invition Link for persian support is : \n https://telegram.me/joinchat/Ch4fUAZ0J0G4LP1-3EwMRg"
+  end
+return {
+  description = "shows persian support", 
+  usage = "!joinSupport : Return persian supports link",
+  patterns = {
+    "^toengsupport$",
+    "^/toengsupport$",
+    "^!toengsupport$",
+    "^>toengsupport$",
+    "^joinSupport$",
+    "^/joinSupport$",
+    "^!joinSupport$",
+    "^>joinSupport$",
+  },
+  run = run
+}
+end
