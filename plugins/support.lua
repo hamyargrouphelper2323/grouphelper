c25cfe7a021410ea515f3f4f14df5c8a02b35d42
BH-YAGHI <iranchild1@gmail.com>
2016-02-04 01:32:02 +0000

    Update support.lua

--- plugins/support.lua
+++ plugins/support.lua
@@ -1,16 +1,12 @@
 do
 
 function run(msg, matches)
-  return " The Invition Link for persian support is : \n https://telegram.me/joinchat/Ch4fUAZ0J0G4LP1-3EwMRg"
+  return " لینک ساپورت نود32 : \n https://telegram.me/joinchat/Ch4fUAZ0J0G4LP1-3EwMRg"
   end
 return {
   description = "shows persian support", 
   usage = "!joinSupport : Return persian supports link",
   patterns = {
-    "^toengsupport$",
-    "^/toengsupport$",
-    "^!toengsupport$",
-    "^>toengsupport$",
     "^joinSupport$",
     "^/joinSupport$",
     "^!joinSupport$",
