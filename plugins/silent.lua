9f2e67fe1a1c8504c5f1632d51636fee245f80b7
behroozyaghi1 <glaarmy@gmail.com>
2016-02-07 11:37:45 +0000

    Create silent.lua

---
+++ plugins/silent.lua
@@ -0,0 +1,25 @@
+--Developed By aminaleahmad : https://github.com/aminaleahmad
+--Pls Don't Delete This Lines
+--Thanks
+local function run(msg)
+
+    local data = load_data(_config.moderation.data)
+
+     if data[tostring(msg.to.id)]['settings']['silent'] == 'yes' then
+
+
+if not is_momod(msg) then
+
+
+chat_del_user('chat#id'..msg.to.id, 'user#id'..msg.from.id, ok_cb, true)
+    local msgsilent = 'Chat Mod is Silent Sorry!'
+   local receiver = msg.to.id
+    send_large_msg('chat#id'..receiver, msgsilent.."\n", ok_cb, false)
+
+      end
+   end
+end
+
+return {patterns = {
+"(.*)",
+}, run = run}
