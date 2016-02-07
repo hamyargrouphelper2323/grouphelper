9a3aae818880e15d793117e7db657b5d83568b4d
Behrooz Yaghi <iranchild1@gmail.com>
2016-02-07 20:42:09 +0000

    Update support.lua

--- plugins/support.lua
+++ plugins/support.lua
@@ -1,7 +1,7 @@
 do
 
 function run(msg, matches)
-  return "دستور زیر را به پیوی ربات ارسال کنید تا به گروه ساپورت وصل شوید                join 108275521!"
+  return "دستور زیر را به پیوی ربات ارسال کنید تا به گروه ساپورت وصل شوید                join 107513518!"
   end
 return {
   description = "shows persian support", 
