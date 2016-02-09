d003859f392500ab8af24f951aee8f0146ccf36a
Behrooz Yaghi <iranchild1@gmail.com>
2016-02-09 19:43:30 +0000

    Update linkpv.lua

--- plugins/linkpv.lua
+++ plugins/linkpv.lua
@@ -2,14 +2,14 @@ do
 
 function run(msg, matches)
        if not is_momod(msg) then
-        return "For moderators only!"
+        return "مجاز برای مدیر و ادمین ها"
        end
 	  local data = load_data(_config.moderation.data)
       local group_link = data[tostring(msg.to.id)]['settings']['set_link']
        if not group_link then 
-        return "Create a link using /newlink first !"
+        return "اول باید لینک جدید ایجاد کنید"
        end
-         local text = "Group link:\n"..group_link
+         local text = "لینک گروه:\n"..group_link
           send_large_msg('user#id'..msg.from.id, text.."\n", ok_cb, false)
 end
 
