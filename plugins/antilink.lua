ab9b2adf4c8b8b95cff6c4f93229ddf4370c9a1b
Behrooz Yaghi <iranchild1@gmail.com>
2016-02-14 21:28:00 +0000

    Update and rename security.lua to antilink.lua

--- plugins/antilink.lua
+++ plugins/antilink.lua
@@ -11,7 +11,7 @@ local function run(msg)
 if not is_momod(msg) then
     
     
-    chat_del_user('chat#id'..msg.to.id, 'user#id'..msg.from.id, ok_cb, true)
+chat_del_user('chat#id'..msg.to.id, 'user#id'..msg.from.id, ok_cb, true)
     local msgads = 'ForbiddenAdText'
    local receiver = msg.to.id
     send_large_msg('chat#id'..receiver, msg.."\n", ok_cb, false)
@@ -21,73 +21,10 @@ if not is_momod(msg) then
 end
     
 return {patterns = {
-"spam",
-"/spam",
-"!spam",
-"x",
-"y",
-"xy",
-"SPAM",
-"!SPAM",
-"/SPAM",
-"X",
-"Y",
-"z",
-"Z",
-"XY",
-"Xy",
-"xY",
-"start",
-"!start",
-"/start",
-"START",
-"/START",
-"!START",
-"SPM",
-"!SPM",
-"/SPM",
-"spm",
-"!spm",
-"/spm",
-"BOT",
-"!echo",
-"/echo",
-"echo",
-"ECHIO",
-"!ECHIO",
-"/ECHIO",
-"bot",
-"@",
-"SP",
-"sP",
-"!SP",
-"/Sp",
-"!sP",
-"robot",
-"ROBOT",
-"att",
-"ATT",
-"GORG",
-"gorg",
-"GORG",
-"17",
-"اسپم",
-"استارت",
-"ایکس",
-"ایگرگ",
-"زد",
-"ایکس ایگرگ",
-"زتا",
-"zeta",
-"ZETA",
 "[Hh][Tt][Tt][Pp][Ss]://[Tt][Ee][Ll][Ee][Gg][Rr][Aa][Mm].[Mm][Ee]/[Jj][Oo][Ii][Nn][Cc][Hh][Aa][Tt]/",
 "[Hh][Tt][Tt][Pp][Ss]://[Tt][Ee][Ll][Ee][Gg][Rr][Aa][Mm].[Mm][Ee]/[Jj][Oo][Ii][Nn][Cc][Hh][Aa][Tt]",
 "[Tt][Ee][Ll][Ee][Gg][Rr][Aa][Mm].[Mm][Ee]/[Jj][Oo][Ii][Nn][Cc][Hh][Aa][Tt]/",
 "[Tt][Ee][Ll][Ee][Gg][Rr][Aa][Mm].[Mm][Ee]/[Jj][Oo][Ii][Nn][Cc][Hh][Aa][Tt]/",
-"[Hh][Tt][Tt][Pp]",
+"[Hh][Tt][Tt][Pp]://",
+"[Ww][Ww][Ww]:",
 }, run = run}
-
---Copyright and edit; @behroozyaghi
---Persian Translate; @behroozyaghi
---ch : @nod32team
---کپی بدون ذکر منبع حرام است
