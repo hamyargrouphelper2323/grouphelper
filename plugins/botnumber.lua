43cda6d6ffbf61c51d302be5b31907a44198b697
Behrooz Yaghi <iranchild1@gmail.com>
2016-02-15 00:36:09 +0000

    Update botnumber.lua

--- plugins/botnumber.lua
+++ plugins/botnumber.lua
@@ -1,7 +1,7 @@
 do
 
 function run(msg, matches)
-send_contact(get_receiver(msg), "Your Bot Phone Number", "NOD32", "BOT", ok_cb, false)
+send_contact(get_receiver(msg), "+13604742767", "NOD32", "BOT", ok_cb, false)
 end
 
 return {
