b0f5550cfc85b54649bf723731af4f55070f0cb0
Behrooz Yaghi <iranchild1@gmail.com>
2016-02-16 00:00:05 +0000

    Update invite.lua

--- plugins/invite.lua
+++ plugins/invite.lua
@@ -1,34 +1,41 @@
 do
-
-local function callback(extra, success, result)
-  vardump(success)
-  vardump(result)
+local function callbackres(extra, success, result) -- Callback for res_user in line 27
+  local user = 'user#id'..result.id
+	local chat = 'chat#id'..extra.chatid
+	if is_banned(result.id, extra.chatid) then -- Ignore bans
+            send_large_msg(chat, 'User is banned.')
+	elseif is_gbanned(result.id) then -- Ignore globall bans
+	    send_large_msg(chat, 'User is globaly banned.')
+	else    
+	    chat_add_user(chat, user, ok_cb, false) -- Add user on chat
+	end
 end
-
-local function run(msg, matches)
-  local user = 158990680
-
-  if matches[1] == "support" then
-    user = 'user#id'..user
+function run(msg, matches)
+  local data = load_data(_config.moderation.data)
+  if not is_realm(msg) then
+    if data[tostring(msg.to.id)]['settings']['lock_member'] == 'yes' and not is_admin(msg) then
+		  return 'Group is private.'
+    end
   end
-
-  -- The message must come from a chat group
-  if msg.to.type == 'chat' then
-    local chat = 'chat#id'..msg.to.id
-    chat_add_user(chat, user, callback, false)
-    return "سازنده ربات وارد گروه شد"
-  else 
-    return 'This isnt a chat group!'
+  if msg.to.type ~= 'chat' then 
+    return
   end
-
+  if not is_momod(msg) then
+    return
+  end
+  --if not is_admin(msg) then -- For admins only !
+    --return 'Only admins can invite.'
+  --end
+	local cbres_extra = {chatid = msg.to.id}
+  local username = matches[1]
+  local username = username:gsub("@","")
+  res_user(username,  callbackres, cbres_extra)
 end
-
 return {
-  description = "support", 
-  patterns = {
-    "^[!/](support)$"
-  }, 
-  run = run 
+    patterns = {
+      "^[!/]invite (.*)$"
+    },
+    run = run
 }
 
 end
